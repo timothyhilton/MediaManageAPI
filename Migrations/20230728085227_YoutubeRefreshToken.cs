dce6a95a6fe5bf733f930ef99d4cb4b7c9d86e47
tim h <recore444@gmail.com>
2023-07-28 18:52:54 +1000

    new database migrations

---
+++ Migrations/20230728085227_YoutubeRefreshToken.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaManageAPI.Migrations
+{
+    public partial class YoutubeRefreshToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "YoutubeRefreshToken",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YoutubeRefreshToken",
+                table: "AspNetUsers");
+        }
+    }
+}
