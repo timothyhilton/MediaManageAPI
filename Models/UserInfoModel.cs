b31e9c7779dc843e07f14a3c203a607096195988
timothyhilton <recore444@gmail.com>
2023-09-28 18:29:03 +1000

    added model for youtube videos + stuff to go with it

---
+++ Models/UserInfoModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediaManageAPI.Models
+{
+    public class UserInfoModel
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Username { get; set; } = null!;
+    }
+}
